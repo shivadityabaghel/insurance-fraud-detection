b3b841f3f4e13a97cf097b57d562aaf4cccdee95
Shivaditya Baghel <89775203+shivadityabaghel@users.noreply.github.com>
2024-05-21 00:08:26 +0530

    Delete scripts directory

--- scripts/deploy.py
+++
@@ -1,8 +0,0 @@
-from brownie import SimpleStorage, accounts
-
-def main():
-    admin = accounts[1]
-    ss = SimpleStorage.deploy({
-        "from": admin,
-    })
-    print("Poicy block is deployed at address:", ss)
